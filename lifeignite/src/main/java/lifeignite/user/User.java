b096083d2f874223b5012de05260a3f7b64bf2a3
최윤호 <choiking10@naver.com>
2017-07-25 19:31:07 +0900

    User 생성자 추가

--- lifeignite/src/main/java/lifeignite/user/User.java
+++ lifeignite/src/main/java/lifeignite/user/User.java
@@ -7,7 +7,12 @@ public class User {
     String id;
     String name;
     String password;
-
+    public User(){}
+    public User(String id, String name, String password){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
     public String getId(){
         return id;
     }
