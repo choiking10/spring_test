d70ee1f6f0200e7660f2978c0fde1b56ea353297
최윤호 <choiking10@naver.com>
2017-07-25 15:34:07 +0900

    1.8 ConnectionMaker => DataSource로 변경

--- lifeignite/src/main/java/user/UserDao.java
+++ lifeignite/src/main/java/user/UserDao.java
@@ -1,5 +1,6 @@
 package user;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,14 +10,14 @@ import java.sql.SQLException;
  * Created by yunho on 2017. 7. 24..
  */
 public class UserDao {
-    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
     public UserDao(){
     }
-    public void setConnectionMaker(ConnectionMaker connectionMaker){
-        this.connectionMaker = connectionMaker;
+    public void setDataSource(DataSource dataSource){
+        this.dataSource = dataSource;
     }
     public void add(User user) throws ClassNotFoundException, SQLException{
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)"
@@ -32,7 +33,7 @@ public class UserDao {
 
     }
     public User get(String id) throws ClassNotFoundException, SQLException{
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?"
