19d1710272a089a2c36a207fdb903983d09f23df
최윤호 <choiking10@naver.com>
2017-07-25 17:42:24 +0900

    2.2 junit 적용

--- lifeignite/src/main/java/lifeignite/UserDaoTest.java
+++ lifeignite/src/main/java/lifeignite/UserDaoTest.java
@@ -1,6 +1,10 @@
 package lifeignite;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import user.User;
@@ -8,10 +12,13 @@ import user.UserDao;
 
 import java.sql.SQLException;
 
-@SpringBootApplication
 public class UserDaoTest {
+	public static void main(String[] args) {
+		JUnitCore.main("lifeignite.UserDaoTest");
+	}
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+	@Test
+	public void addAndGet() throws ClassNotFoundException, SQLException{
 		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
 		UserDao dao = context.getBean("userDao", UserDao.class);
@@ -23,18 +30,9 @@ public class UserDaoTest {
 
 		dao.add(user);
 
-		System.out.println(user.getId() + " 등록 성공");
-
 		User user2 = dao.get(user.getId());
 
-		if(!user.getName().equals(user2.getName())){
-			System.out.println("테스트 실패 (name)");
-		}
-		if(!user.getPassword().equals(user2.getPassword())){
-			System.out.println("테스트 실패 (password)");
-		}else {
-			System.out.println("조회 테스트 성공");
-		}
-
+		assertThat(user2.getName(),is(user.getName()));
+		assertThat(user2.getPassword(),is(user.getPassword()));
 	}
 }
